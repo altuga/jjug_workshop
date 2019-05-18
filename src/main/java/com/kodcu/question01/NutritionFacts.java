32113ea9b0b864e743f69dad55c83ec2a9944fdb
AltugAltintas <altuga@gmail.com>
2019-05-18 10:26:37 +0900

    solution 3 Builder pattern

--- src/main/java/com/kodcu/question01/NutritionFacts.java
+++ src/main/java/com/kodcu/question01/NutritionFacts.java
@@ -2,11 +2,10 @@ package com.kodcu.question01;
 
 /**
  * Altug Bilgin Altintas
- * 
+ *
  * Effective Java Workshop
- * 
- * Email : altug@kodcu.com
- * Twitter : @altugaltintas
+ *
+ * Email : altug@kodcu.com Twitter : @altugaltintas
  */
 
 /*
@@ -14,54 +13,79 @@ TODO:
  1 - Initialize (create) the class according to the fields below - 5 minutes
  2 - Instructor will show the solutions
  3 - Refactor and ask questions
-*/
+ */
 public class NutritionFacts {
-    private final int servingSize ; // must
-    private final int servings   ; // must
-    private  int calories ; // optional
-    private  int fat ;  // optional
-    private  int sodium ; // optional
-    private  int carbohydrate = 0; // optional
 
-    public NutritionFacts(int servingSize, int servings) {
-        this.servingSize = servingSize;
-        this.servings = servings;
-    }
+    private final int servingSize; // must
+    private final int servings; // must
+    private final int calories; // optional
+    private final int fat;  // optional
+    private final int sodium; // optional
+    private final int carbohydrate = 0; // optional
 
-    public void setCalories(int calories) {
-        this.calories = calories;
-    }
-
-    public void setFat(int fat) {
-        this.fat = fat;
-    }
-
-    public void setSodium(int sodium) {
-        this.sodium = sodium;
-    }
-
-    public void setCarbohydrate(int carbohydrate) {
-        this.carbohydrate = carbohydrate;
+    private NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.servings = builder.servings;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
     }
 
     
+
+    public static class Builder {
+
+        private final int servingSize; // must
+        private final int servings; // must
+        private  int calories; // optional
+        private  int fat;  // optional
+        private  int sodium; // optional
+        private  int carbohydrate = 0; // optional
+
+        public Builder(int servingSize, int servings) {
+            this.servingSize = servingSize;
+            this.servings = servings;
+        }
+
+        public Builder setCalories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder setFat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder setSodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder setCarbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+        
+        public NutritionFacts build() {
+            return new NutritionFacts(this);
+        }
+        
+        
+        
+        
+    }
 
     @Override
     public String toString() {
         return "NutritionFacts{" + "servingSize=" + servingSize + ", servings=" + servings + ", calories=" + calories + ", fat=" + fat + ", sodium=" + sodium + ", carbohydrate=" + carbohydrate + '}';
     }
 
- 
-    
-
     public static void main(String[] args) {
-        NutritionFacts nutritionFacts = new NutritionFacts(10, 90);
-        nutritionFacts.setCarbohydrate(100);
-        nutritionFacts.setFat(10);
+        NutritionFacts nutritionFacts = new NutritionFacts.Builder(10,10)
+                .setCalories(100)
+                .setSodium(10).setFat(790).build();
         
-        nutritionFacts.setFat(11);
-        System.out.println(nutritionFacts);
     }
-    
-    
+
 }
